2ffa7399503d741a7193d88de01b4b1ebd2570b6
ben <bjelinek42@gmail.com>
2022-03-10 12:08:43 -0700

    add controllers and routes practice

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -5,4 +5,30 @@ class NamesController < ApplicationController
       render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
     end
   end
+
+  def segment
+    render json: {message: "You live in #{params[:city]}, #{params[:state]}."}
+  end
+
+  def guess_number
+    user_guess = params[:guess].to_i
+    answer = 32
+    if user_guess > answer
+      render json: the_message = "Your guess is too high"
+    elsif user_guess < answer
+      render json: the_message = "Your guess is too low"
+    elsif user_guess == answer
+      render json: the_message = "CONGRATULATIONS!!!!!!!"
+    end
+  end
+
+  def validate_login
+    username = params[:username]
+    password = params[:password]
+    if username == "hugh" && password == "swordfish"
+      render json: "valid credentials"
+    else
+      render json: "invalid credentials"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,14 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "/name" => "names#what_is_your_name"
+
+  # get "/segment_param/:food/:calories" => "names#segment"
+
+  get "/segment_param/:city/:state/:county" => "names#segment"
+
+  post "/guess_number" => "names#guess_number"
+  get "/guess_number" => "names#guess_number"
+
+  post "/login" => "names#validate_login"
   
 end
